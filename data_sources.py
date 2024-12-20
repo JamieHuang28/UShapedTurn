8445186f42ab79b672030011cca0e7a5f92956bb
JamieWong <huangzm28@163.com>
2024-12-20 14:34:05 +0800

    format code; add input json files.

--- data_sources.py
+++ data_sources.py
@@ -8,21 +8,25 @@ from bokeh.palettes import Spectral7
 
 import math
 
+
 class RoadCurbDataSource:
     def __init__(self, defualt_lane_width):
         self.x = np.array([-defualt_lane_width, -defualt_lane_width])
         self.y = np.array([-10.0, 10.0])
         self.source = ColumnDataSource(data=self.get())
-    
+
     def updateData(self, x):
         self.x = np.array([x, x])
         self.source.data = self.get()
-    
+
     def get(self):
         return dict(x=self.x, y=self.y)
-    
+
     def registerRender(self, plot):
-        plot.line('x', 'y', source=self.source, line_width=3, line_alpha=0.6, color="#0000ff")
+        plot.line(
+            "x", "y", source=self.source, line_width=3, line_alpha=0.6, color="#0000ff"
+        )
+
 
 class PoseDataSource:
     def __init__(self, default_dict):
@@ -31,64 +35,87 @@ class PoseDataSource:
         self.yaw = default_dict.yaw
         self.color = default_dict.color
         self.source = ColumnDataSource(data=self.get())
-    
+
     def updateData(self, x, y, yaw):
         self.x = x
         self.y = y
         self.yaw = yaw
         self.source.data = self.get()
-    
+
     def get(self):
-        return dict(start_x=[self.x], start_y=[self.y], end_x=[self.x + math.cos(self.yaw)], end_y=[self.y+math.sin(self.yaw)])
-    
+        return dict(
+            start_x=[self.x],
+            start_y=[self.y],
+            end_x=[self.x + math.cos(self.yaw)],
+            end_y=[self.y + math.sin(self.yaw)],
+        )
+
     def registerRender(self, p):
-        p.add_layout(Arrow(end=NormalHead(size=10, fill_color=self.color, line_color=self.color), source=self.source, x_start='start_x', y_start='start_y', x_end='end_x', y_end='end_y'))
+        p.add_layout(
+            Arrow(
+                end=NormalHead(size=10, fill_color=self.color, line_color=self.color),
+                source=self.source,
+                x_start="start_x",
+                y_start="start_y",
+                x_end="end_x",
+                y_end="end_y",
+            )
+        )
 
 
 from bokeh.models import Dot
+
+
 class TurnAnchorDataSource:
     def __init__(self):
         self.x = [0.0]
         self.y = [0.0]
         self.sizes = [20.0]
         self.source = ColumnDataSource(self.get())
-    
+
     def updateData(self, x, y):
         self.x = np.array([0.0]) + x
         self.y = np.array([0.0]) + y
         self.source.data = self.get()
-    
+
     def get(self):
         return dict(x=self.x, y=self.y, sizes=self.sizes)
-    
+
     def registerRender(self, plot):
         glyph = Dot(x="x", y="y", size="sizes", line_color="#dd1c77", fill_color=None)
         plot.add_glyph(self.source, glyph)
 
+
 from drive_path import DrivePath
+
+
 class DrivePathDataSource:
     def __init__(self, drive_path: DrivePath):
         self.drive_path = drive_path
         self.source = ColumnDataSource(self.get())
-    
+
     def get(self):
         # return self.drive_path.getPointsAndArrow()
-        
+
         return self.drive_path.getSegments()
-    
+
     def updateData(self):
         # no update is required
         pass
-    
+
     def registerRender(self, p):
         # p.add_layout(Arrow(end=OpenHead(size=3), source=self.source, x_start='x_start', y_start='y_start', x_end='x_end_arrow', y_end='y_end_arrow'))
         # p.line(source=self.source, x='x_start', y='y_start', line_width=3, line_color="#ffff00")
-        
+
         line_opts = dict(
-            line_width=5, line_color='colors', line_alpha=0.9,
-            hover_line_color='colors', hover_line_alpha=1.0
+            line_width=5,
+            line_color="colors",
+            line_alpha=0.9,
+            hover_line_color="colors",
+            hover_line_alpha=1.0,
         )
-        p.multi_line(source=self.source, xs='segment_xs', ys='segment_ys', **line_opts)
+        p.multi_line(source=self.source, xs="segment_xs", ys="segment_ys", **line_opts)
+
 
 class TrajDataSource:
     def __init__(self):
@@ -96,15 +123,27 @@ class TrajDataSource:
         self.y = [0.0, 1.0, 2.0]
         self.colors = ["ff0000", "00ff00", "#0000ff"]
         self.source = ColumnDataSource(self.get())
-    
+
     def get(self):
-        return dict(x=self.x, y=self.y, sizes=np.ones(len(self.x)) * 5, colors=self.colors)
-    
+        return dict(
+            x=self.x, y=self.y, sizes=np.ones(len(self.x)) * 5, colors=self.colors
+        )
+
     def updateData(self, traj):
         self.x = [pose.x for pose in traj]
         self.y = [pose.y for pose in traj]
-        self.colors = [Spectral7[::-1][pose.projection2drive_path_segment_idxs] for pose in traj]
+        self.colors = [
+            Spectral7[::-1][pose.projection2drive_path_segment_idxs] for pose in traj
+        ]
         self.source.data = self.get()
-    
+
     def registerRender(self, plot):
-        plot.circle(source=self.source, x="x", y="y", size="sizes", color="peachpuff", fill_color='colors', alpha=0.7)
+        plot.circle(
+            source=self.source,
+            x="x",
+            y="y",
+            size="sizes",
+            color="peachpuff",
+            fill_color="colors",
+            alpha=0.7,
+        )
--- vehicle_model.py
+++ vehicle_model.py
@@ -4,31 +4,34 @@ import numpy as np
 
 # from abc import ABC, abstractclassmethod
 
+
 class VehicleModel:
-    def __init__(self, wheel_base = 3.0, max_delta = 0.5):
+    def __init__(self, wheel_base=3.0, max_delta=0.5):
         self.wheel_base = wheel_base
         self.max_delta = max_delta
-        self.pose = EasyDict({'x': 0.0, 'y': 0.0, 'yaw': 0.0})
-    
+        self.pose = EasyDict({"x": 0.0, "y": 0.0, "yaw": 0.0})
+
     def initPose(self, x, y, yaw):
         self.pose.x = x
         self.pose.y = y
         self.pose.yaw = yaw
-    
+
     def drive(self, velocity: float, delta: float, dt: float):
         self.pose.x = self.pose.x + math.cos(self.pose.yaw) * velocity * dt
         self.pose.y = self.pose.y + math.sin(self.pose.yaw) * velocity * dt
-        self.pose.yaw = self.pose.yaw + math.tan(delta) / self.wheel_base * velocity * dt
-    
+        self.pose.yaw = (
+            self.pose.yaw + math.tan(delta) / self.wheel_base * velocity * dt
+        )
+
     def path(self, distance, delta):
-        trace = EasyDict({'ts':[],'xs':[], 'ys':[], 'yaws':[]})
+        trace = EasyDict({"ts": [], "xs": [], "ys": [], "yaws": []})
         kVelocity = 1.0
         kTime = distance / kVelocity
         kDt = 0.1
         for i in np.arange(0, kTime, kDt):
             # drive on step for kDt time period
             self.drive(kVelocity, delta, kDt)
-            
+
             trace.ts.append(i)
             trace.xs.append(self.pose.x)
             trace.ys.append(self.pose.y)
@@ -38,6 +41,7 @@ class VehicleModel:
 
 if __name__ == "__main__":
     import matplotlib
+
     # matplotlib.use('TkAgg')
     import matplotlib.pyplot as plt
 
@@ -45,8 +49,8 @@ if __name__ == "__main__":
     vm = VehicleModel(3.0, 0.5)
     trace = vm.path(1.0, 0.5)
     print(trace)
-    fig, axs = plt.subplots(2,1)
+    fig, axs = plt.subplots(2, 1)
     axs[0].plot(trace.xs, trace.ys)
-    axs[0].axis('equal')
+    axs[0].axis("equal")
     axs[1].plot(trace.ts, trace.yaws)
-    plt.savefig('vehicle_model.png')
+    plt.savefig("vehicle_model.png")
